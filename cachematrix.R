4cdf56b59047bd5fcd5507ece3544df092c9c627
Sai Preteek Medi <saipreteek.medi@hexacorp.com>
2015-04-26 15:44:50 -0400

    compute inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix<-function(b=matrix()) {
+  #this function is a version of makeVector to inverse matrices.
+  #m1 will be null until receiving a matrix, when a matrix enters as an argument, it will be inverted.
+  #by cacheSolve. get and set will help to check whether an inverse value can be found in the function environment.
+  m1<-NULL
+  set <-function(n1){
+    b<<-n1
+    m1<<-NULL    
+  }
+  get<-function() b
+  setinverse<-function(inverse) m1<<-inverse
+  getinverse<-function() m1
+  
+  list(set=set,get=get,getinverse=getinverse,setinverse=setinverse,cacheSolve=cacheSolve)
+  
+}
 
-## Write a short comment describing this function
+cacheSolve<-function(b,...){
+  
+  #cacheSolve checks for m1 where "data" is the cached value of m1 inversed.
+  # if not found in cached data, it will conduct solve on the matrix to get inverse
+  m1<-b$getinverse()
+  if(!is.null(m1)) {
+    message("getting cached data")  
+    return(m1)
+    
+  }
+  data<-b$get()
+  m1<-solve(data,...)
+  b$setinverse(m1)
+  m1    
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+testcache<-function() {
+  #this function helped me test what values will appear for d in different environments
+  d<<-3.14
+  print(d)
+  print(d)
+  d<-42
+  print(d)
+  
+  print(d)
 }
 
 
-## Write a short comment describing this function
+plusFunctions<-function() {
+  plustwo<-function(y) {
+    x<-y+2
+    return(x)
+  }
+  
+  plusthree<-function(y) {
+    x<-y+3
+    return(x)
+    
+  }  
+  list(plustwo=plustwo, plusthree=plusthree)
+} 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
